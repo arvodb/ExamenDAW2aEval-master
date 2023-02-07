d882cfaa177981a4b569585276f62cf0697efa24
arvodb <daboma@floridauniversitaria.es>
2023-02-07 12:44:13 +0100

    first commit

---
+++ test/enanaTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+include './src/Enana.php';
+
+class EnanaTest extends TestCase {
+
+    public function testHeridaLeveVive() {
+       
+        #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
+        #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
+        $enana = new Enana('Meredith',30,'viva');
+        $this->assertEquals([20,'viva'],$enana->heridaLeve());
+    }
+
+    public function testHeridaLeveMuere() {
+       
+        #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
+        #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
+        $enana = new Enana('Meredith',5,'viva');
+        $this->assertEquals([-5,'muerta'],$enana->heridaLeve());
+
+    }
+
+    public function testHeridaGrave() {
+       
+        #Se probará el efecto de una herida grave a una Enana con una situación de viva.
+        #Se tendrá que probar que la vida es 0 y además que su situación es limbo
+        $enana = new Enana('Meredith',5,'viva');
+        $this->assertEquals([0,'limbo'],$enana->heridaGrave());
+
+
+    }
+    
+    public function testPocimaRevive() {
+       
+        #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
+        #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
+        $enana = new Enana('Meredith',-5,'muerta');
+        $this->assertEquals([5,'viva'],$enana->pocima());
+
+
+    }
+
+    public function testPocimaExtraLimbo() {
+       
+        #Se probará el efecto de administrar una pócima Extra a una Enana en el limbo.
+        #Se tendrá que probar que la vida es 50 y la situación ha cambiado a viva.
+        $enana = new Enana('Meredith',0,'limbo');
+        $this->assertEquals([50,'viva'],$enana->pocimaExtra());
+    }
+}
+
+
+?>
